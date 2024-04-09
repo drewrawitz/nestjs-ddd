17baecb5989e56910b06bcb7c037226c57b42826
Drew Rawitz <email@drewrawitz.com>
2024-04-09 18:19:44 -0400

    using the Fastify platform adapter

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,9 +1,16 @@
 import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 import { EnvService } from './infrastructure/env/env.service';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
   const configService = app.get(EnvService);
 
   await app.listen(configService.get('PORT'));
