51b55a6758e086fac7fa54c66eb9ede6b08e2a3d
Drew Rawitz <email@drewrawitz.com>
2024-05-11 18:43:28 -0400

    adding 2fa dialogs

--- apps/web/src/components/add-2fa-button.tsx
+++ apps/web/src/components/add-2fa-button.tsx
@@ -8,21 +8,28 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { VerificationRequiredDialog } from "./dialogs/verification-required";
+import { Dialog, DialogTrigger } from "@/components/ui/dialog";
 
 function Add2faButton() {
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button>Add authentication method</Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent className="w-56" align="end" forceMount>
-        <DropdownMenuGroup>
-          <DropdownMenuItem>
-            <button>Use an authenticator app</button>
-          </DropdownMenuItem>
-        </DropdownMenuGroup>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <Dialog>
+      <VerificationRequiredDialog />
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button>Add authentication method</Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="w-56" align="end" forceMount>
+          <DropdownMenuGroup>
+            <DropdownMenuItem>
+              <DialogTrigger asChild>
+                <button>Use an authenticator app</button>
+              </DialogTrigger>
+            </DropdownMenuItem>
+          </DropdownMenuGroup>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </Dialog>
   );
 }
 
