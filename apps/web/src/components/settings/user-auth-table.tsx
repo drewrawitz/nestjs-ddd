335bf15c03b6cb22abf7c11503100c6521b46f2b
Drew Rawitz <email@drewrawitz.com>
2024-05-20 11:58:31 -0400

    adding a dropdown for the 2FA table

--- apps/web/src/components/settings/user-auth-table.tsx
+++ apps/web/src/components/settings/user-auth-table.tsx
@@ -10,6 +10,14 @@ import {
   TableRow,
   TableCaption,
 } from "@/components/ui/table";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import { useCurrentUserQuery } from "@/lib/features/auth/auth.hooks";
 import { MFAType } from "@app/prisma/client";
 import { formattedDate } from "@app/shared";
@@ -22,35 +30,61 @@ export default function UserAuthTable() {
   const { data: user } = useCurrentUserQuery();
 
   return (
-    <Table>
-      <TableHeader>
-        <TableRow>
-          <TableHead className="w-[250px]">Method</TableHead>
-          <TableHead>Date added</TableHead>
-          <TableHead className=""></TableHead>
-        </TableRow>
-      </TableHeader>
-      {user?.mfa && user?.mfa.length < 1 ? (
-        <TableCaption>
-          You don&apos;t have any authentication methods added.
-        </TableCaption>
-      ) : (
-        <TableBody>
-          {user?.mfa.map((m) => {
-            return (
-              <TableRow key={m.type}>
-                <TableCell className="font-medium">
-                  {mfaMapping[m.type] ?? "—"}
-                </TableCell>
-                <TableCell>{formattedDate(m.createdAt)}</TableCell>
-                <TableCell className="text-right flex items-center justify-end">
-                  <Ellipsis />
-                </TableCell>
-              </TableRow>
-            );
-          })}
-        </TableBody>
-      )}
-    </Table>
+    <>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[250px]">Method</TableHead>
+            <TableHead>Date added</TableHead>
+            <TableHead className=""></TableHead>
+          </TableRow>
+        </TableHeader>
+        {user?.mfa && user?.mfa.length < 1 ? (
+          <TableCaption>
+            You don&apos;t have any authentication methods added.
+          </TableCaption>
+        ) : (
+          <TableBody>
+            {user?.mfa.map((m) => {
+              return (
+                <TableRow key={m.type}>
+                  <TableCell className="font-medium">
+                    {mfaMapping[m.type] ?? "—"}
+                  </TableCell>
+                  <TableCell>{formattedDate(m.createdAt)}</TableCell>
+                  <TableCell className="text-right flex items-center justify-end">
+                    <DropdownMenu>
+                      <DropdownMenuTrigger asChild>
+                        <Button variant="ghost">
+                          <Ellipsis />
+                        </Button>
+                      </DropdownMenuTrigger>
+                      <DropdownMenuContent
+                        className="w-32"
+                        align="end"
+                        forceMount
+                      >
+                        <DropdownMenuGroup>
+                          <DropdownMenuItem className="cursor-pointer text-red-600">
+                            Remove
+                          </DropdownMenuItem>
+                        </DropdownMenuGroup>
+                      </DropdownMenuContent>
+                    </DropdownMenu>
+                  </TableCell>
+                </TableRow>
+              );
+            })}
+          </TableBody>
+        )}
+      </Table>
+      <p className="mt-6 text-gray-300 text-sm text-center">
+        If you lose your mobile device or security key, you can{" "}
+        <button className="text-amber-600 hover:underline">
+          generate a backup code
+        </button>{" "}
+        to sign in to your account.
+      </p>
+    </>
   );
 }
